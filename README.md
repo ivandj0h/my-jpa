939c6d035af6d6905fc481d9d3b032fd89378645
ivandjoh <ivandi.djoh@gmail.com>
2023-02-05 08:23:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# My JPA
+
+## Description
+This is a simple example of a JPA project.
---
+++ src/main/java/com/ivandjoh/jpa/JpaApp.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApp.class, args);
+	}
+
+}
